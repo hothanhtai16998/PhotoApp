5ffd9121ed20c9062884e36952425b54b4cfac5e
hothanhtai16998 <appstoreusa998@gmail.com>
2025-11-12 10:20:33 +0700

    Prepare to deploy app

--- backend/src/libs/environment.js
+++ backend/src/libs/environment.js
@@ -1,14 +1,12 @@
 import "dotenv/config";
 
 export const env = {
-
 	PORT: process.env.PORT,
-
 	MONGODB_URI: process.env.MONGODB_URI,
-
 	ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
-
 	CLIENT_URL: process.env.CLIENT_URL,
-
-
+	PHOTO: process.env.PHOTO,
+	CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+	CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,15 @@
 import express from "express";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import { CONNECT_DB } from "./config/mongodb.js";
 import { env } from "./libs/environment.js";
 import cors from "cors";
+import path from "path";
 
 //import route
 import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 
-
 import { protectedRoute } from "./middlewares/authMiddleware.js";
-
 
 const app = express();
 
@@ -19,29 +18,42 @@ const PORT = env.PORT || 4001;
 //middlewares
 app.use(express.json());
 app.use(cookieParser()); // để lưu token vào cookie
+const __dirname = path.resolve();
 
 //khi backend và frontend không cùng folder, phải enable CORS để truy cập từ client-side
-const allowedOrigins = [env.CLIENT_URL, 'http://localhost:5173'];
-app.use(cors({ 
-    origin: (origin, callback) => {
-        // allow requests with no origin (like mobile apps or curl requests)
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }, 
-    credentials: true 
-}));
+const allowedOrigins = [env.CLIENT_URL, "http://localhost:5173"];
+app.use(
+	cors({
+		origin: (origin, callback) => {
+			// allow requests with no origin (like mobile apps or curl requests)
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				const msg =
+					"The CORS policy for this site does not allow access from the specified Origin.";
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+		credentials: true,
+	})
+);
 
 //public route: có thể truy cập mà không cần đăng nhập
-app.use('/api/auth', authRoute); 
+app.use("/api/auth", authRoute);
 
 //private route: cần đăng nhập để truy cập
 app.use(protectedRoute);
-app.use('/api/users', userRoute); 
+app.use("/api/users", userRoute);
 
+//deployment
+if (env.PHOTO_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.get("*", (_, res) => {
+		res.sendFile(
+			path.resolve(__dirname, "../frontend", "dist", "index.html")
+		);
+	});
+}
 
 //kết nối database
 CONNECT_DB().then(() => {
