d569504e528570258965bfd537e4d6934aa5b1f9
hothanhtai16998 <appstoreusa998@gmail.com>
2025-11-20 05:10:47 +0700

    fix search

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,26 @@
+import { useEffect } from "react";
 import Header from "../components/Header";
 import ImageGrid from "@/components/ImageGrid";
 import './HomePage.css';
 import Slider from "@/components/Slider";
+import { useImageStore } from "@/stores/useImageStore";
 
 function HomePage() {
+    const { currentSearch } = useImageStore();
+    
+    // Scroll to top when search is activated to show results immediately
+    useEffect(() => {
+        if (currentSearch) {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+    }, [currentSearch]);
+    
     return (
         <>
             <Header />
             <main className="homepage">
-                <Slider />
+                {/* Hide Slider when user is searching to show ImageGrid immediately */}
+                {!currentSearch && <Slider />}
                 <ImageGrid />
             </main>
         </>
